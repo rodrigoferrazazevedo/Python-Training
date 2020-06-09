084c3c4c9c0034d716204664c68bfd740e2dc528
Rodrigo Ferraz Azevedo <rodrigo.azevedo@sintesoft.com.br>
2020-06-09 11:40:46 -0300

    Using class-based views

--- django-blog-example/blog/urls.py
+++ django-blog-example/blog/urls.py
@@ -4,8 +4,10 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    # post views
-    path('', views.post_list, name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
-    views.post_details, name='post_detail'),
+        # post views
+        #     # path('', views.post_list, name='post_list'),
+             path('', views.PostListView.as_view(), name='post_list'),
+             path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+                  views.post_detail,
+                 name='post_detail'),
 ]
--- django-blog-example/blog/views.py
+++ django-blog-example/blog/views.py
@@ -1,6 +1,8 @@
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
+
 
 def post_list(request):
     object_list = Post.published.all()
@@ -15,6 +17,12 @@ def post_list(request):
     return render(request, 'blog/post/list.html',{'page': page, 'posts': posts})
 
 
-def post_details(request, year, month, day, post):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html',{'post': post})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
