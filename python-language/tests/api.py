e071350e05bf43308bf7ae971230e65150319fdb
Rodrigo Ferraz Azevedo <rodrigo.azevedo@sintesoft.com.br>
2020-08-12 16:24:56 -0300

    Updated api.py test example

--- python-language/tests/api.py
+++ python-language/tests/api.py
@@ -3,3 +3,23 @@ import csv
 from copy import deepcopy
 
 from marshmallow import Schema, fields, pre_load
+from marshmallow.validate import Length, Range
+
+class UserSchema(Schema):
+    email = fields.Email(required=True)
+    name = fields.String(required=True, validate=Length(min=1))
+    age = fields.Integer(required=True, validate=Range(min=18, max=65))
+    role = fields.String()
+
+    @pre_load(pass_many=False)
+    def strip_name(self, data):
+        data_copy = deepcopy(data)
+
+        try:
+            data_copy['name'] = data_copy['name'].strip()
+        except (AttributeError, KeyError, TypeError):
+            pass
+
+        return data_copy
+
+schema = UserSchema()
