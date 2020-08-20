8445ecf56408af06b5f00b3dd83cf80ad8214df3
Rodrigo Ferraz Azevedo <rodrigo.azevedo@sintesoft.com.br>
2020-08-20 15:37:31 -0300

    django-blog-example: added the tagging funcionality

--- django-blog-example/blog/models.py
+++ django-blog-example/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 # Manager
 class PublishedManager(models.Manager):
@@ -31,6 +32,7 @@ class Post(models.Model):
 
     objects = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', args =[self.publish.year, self.publish.month, self.publish.day, self.slug])
--- django-blog-example/blog/views.py
+++ django-blog-example/blog/views.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
@@ -24,8 +25,14 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share.html', {'post':post, 'form':form, 'sent':sent})
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
     try:
@@ -34,7 +41,7 @@ def post_list(request):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html',{'page': page, 'posts': posts})
+    return render(request, 'blog/post/list.html',{'page': page, 'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
