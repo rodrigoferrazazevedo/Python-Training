814dc4e8984aee473c18c16e041d62320705fe1f
Rodrigo Ferraz Azevedo <rodrigo.azevedo@sintesoft.com.br>
2020-08-26 17:06:27 -0300

    django-blog-examploe: sitemap example

--- django-blog-example/blog/urls.py
+++ django-blog-example/blog/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
+from .feeds import LatestPostsFeed
+
+sitemaps = {
+     'posts': PostSitemap,
+}
 
 app_name = 'blog'
 
@@ -11,5 +18,6 @@ urlpatterns = [
                   views.post_detail,
                  name='post_detail'),
              path('<int:post_id>/share/', views.post_share, name='post_share'),
-             path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+             path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'), 
+             path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
