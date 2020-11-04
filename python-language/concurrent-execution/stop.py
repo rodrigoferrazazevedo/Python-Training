4127b80de01608052a0e15ee0b3e9f719f84891e
Rodrigo Ferraz Azevedo <eu@rodrigoazevedo.com>
2020-11-04 19:39:03 -0300

    Some concurrent execution examples

---
+++ python-language/concurrent-execution/stop.py
@@ -0,0 +1,25 @@
+import threading
+from time import sleep
+
+class Fibo(threading.Thread):
+    def __init__(self, *a, **kwa):
+        super().__init__(*a, **kwa)
+        self._running = True
+
+    def stop(self):
+        self._running = False
+
+    def run(self):
+        a, b = 0, 1
+        while self._running:
+            print(a, end=' ')
+            a, b = b, a + b
+            sleep(0.07)
+        print()
+
+fibo = Fibo()
+fibo.start()
+sleep(1)
+fibo.stop()
+fibo.join()
+print('All done.')
