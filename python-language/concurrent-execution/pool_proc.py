6b0025d7d750851267e4d53f41289252321e1029
Rodrigo Ferraz Azevedo <rodrigo.azevedo@sintesoft.com.br>
2021-08-02 21:45:27 -0300

    python-language: concurrent execution examples

---
+++ python-language/concurrent-execution/pool_proc.py
@@ -0,0 +1,17 @@
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from random import randint
+from time import sleep
+
+def run(name):
+    sleep(.05)
+    value = randint(0, 10**2)
+    print(f'Hi, I am {name} and my value is {value}')
+    return (name, value)
+
+with ProcessPoolExecutor(max_workers=3) as executor:
+    futures = [
+        executor.submit(run, f'P{name}') for name in range(5)
+    ]
+    for future in as_completed(futures):
+        name, value = future.result()
+        print(f'Process {name} returned {value}')
